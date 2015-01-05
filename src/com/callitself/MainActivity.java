6dd79f749511050b50f98e3092108a439977ce13
zdssong <601591382@qq.com>
2015-01-06 01:21:07 +0800

    已经可以打电话了

--- src/com/callitself/MainActivity.java
+++ src/com/callitself/MainActivity.java
@@ -1,15 +1,42 @@
 package com.callitself;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+	private Button call;
+	private Button hangUp;
+	private EditText phoneNumber;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		InitWidget();
+
+		call.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (!phoneNumber.getText().toString().equals("")) {
+					Intent intent = new Intent(Intent.ACTION_CALL, Uri
+							.parse("tel:" + phoneNumber.getText()));
+					startActivity(intent);
+				}
+			}
+		});
+	}
+
+	private void InitWidget() {
+		call = (Button) findViewById(R.id.call);
+		phoneNumber = (EditText) findViewById(R.id.number);
 	}
 }
