5259b0757cc751fe2ea247387178ba1652febf5e
690312856 <690312856@qq.com>
2015-01-06 15:44:55 +0800

    熟悉了NumberPicker

--- src/com/callitself/MainActivity.java
+++ src/com/callitself/MainActivity.java
@@ -8,18 +8,29 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnValueChangeListener {
 
 	private Button call;
 	private Button hangUp;
 	private EditText phoneNumber;
+	private NumberPicker timePicker;
+	private NumberPicker numberPicker;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		InitWidget();
+		initWidget();
+		initListener();
+
+		timePicker.setMaxValue(60);
+		timePicker.setMinValue(0);
+
+		numberPicker.setMaxValue(10);
+		numberPicker.setMinValue(0);
 
 		call.setOnClickListener(new OnClickListener() {
 
@@ -35,8 +46,27 @@ public class MainActivity extends Activity {
 		});
 	}
 
-	private void InitWidget() {
+	private void initWidget() {
 		call = (Button) findViewById(R.id.call);
 		phoneNumber = (EditText) findViewById(R.id.number);
+		timePicker = (NumberPicker) findViewById(R.id.timePicker);
+		numberPicker = (NumberPicker) findViewById(R.id.numberpicker);
+	}
+
+	private void initListener() {
+		timePicker.setOnValueChangedListener(this);
+		numberPicker.setOnValueChangedListener(this);
+	}
+
+	@Override
+	public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+		// TODO Auto-generated method stub
+		if (picker == timePicker)
+			System.out.println("timePicker");
+		else if (picker == numberPicker)
+			System.out.println("numberPicker");
+		else
+			System.out.println("nothing");
+
 	}
 }
